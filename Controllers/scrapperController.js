881bbd7e705e883abde20ce1974d79e9f73f36a7
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-10-02 17:53:01 +0530

    Fetch titles

--- Controllers/scrapperController.js
+++ Controllers/scrapperController.js
@@ -1,5 +1,16 @@
+import { scrapData } from "../Services/puppeteerService.js";
 
+export const fetchScrapedData = async (req, res) => {
+  const keyword = req.query.keyword;
 
-export const fetchScrapedData = async(req,res) => {
-    
-}
+  if (!keyword) {
+    return res.status(400).json({ error: "Keyword is required" });
+  }
+
+  try {
+    const results = await scrapData(keyword);
+    res.json(results);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to scrape data" });
+  }
+};
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
+import router from "./Routes/scrapperRoute.js";
+
 
 const app = express();
+
+app.use(router);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
