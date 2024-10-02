c1af4ffabfa2054ebd4e91e45f06b9c46030463b
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-10-02 14:31:39 +0530

    add puppeteer launch and page navigation

--- Services/puppeteerService.js
+++ Services/puppeteerService.js
@@ -1,1 +1,18 @@
-//puppeteer Service
+import puppeteer from "puppeteer";
+
+export const scrapData = async (searchKeyword) => {
+
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({ headless: true });
+
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL.
+  await page.goto(`https://ikman.lk/en/ads?query=${searchKeyword}`);
+
+  
+
+
+
+
+};
