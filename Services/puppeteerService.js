1b50c99fcc712ae101c20cd18f530c089c9ad203
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2024-10-02 14:49:11 +0530

    scrape the title data from the page

--- Services/puppeteerService.js
+++ Services/puppeteerService.js
@@ -1,7 +1,6 @@
 import puppeteer from "puppeteer";
 
 export const scrapData = async (searchKeyword) => {
-
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({ headless: true });
 
@@ -11,9 +10,23 @@ export const scrapData = async (searchKeyword) => {
   await page.goto(`https://ikman.lk/en/ads?query=${searchKeyword}`);
 
   // Wait for the listings to load
-  await page.waitForSelector('.list--3NxGO');
+  await page.waitForSelector(".list--3NxGO");
+
+  const items = await page.evaluate(() => {
+    const data = [];
+    const itemList = document.querySelectorAll(".list--3NxGO > li");
+
+    itemList.forEach((item) => {
+      const title = item.querySelector(".heading--2eONR")
+        ? item.querySelector(".heading--2eONR").innerText
+        : "";
+
+      data.push({ title });
+    });
+    return data;
+  });
 
 
-
-
+  await browser.close();
+  return items;
 };
